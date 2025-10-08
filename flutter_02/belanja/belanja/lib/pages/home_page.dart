a3edf3fb231a35a7db1202304358f144675d2af8
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-10-08 07:10:22 +0700

    Praktikum 5: Membangun Navigasi di Flutter - Langkah 7: Menambahkan Aksi pada ListView

--- flutter_02/belanja/belanja/lib/pages/home_page.dart
+++ flutter_02/belanja/belanja/lib/pages/home_page.dart
@@ -22,19 +22,25 @@ class HomePage extends StatelessWidget {
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
-            return Card(
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                child: Row(
-                  children: [
-                    Expanded(child: Text(item.name)),
-                    Expanded(
-                      child: Text(
-                        item.price.toString(),
-                        textAlign: TextAlign.end,
+            
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/item');
+              },
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                        child: Text(
+                          item.price.toString(),
+                          textAlign: TextAlign.end,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
