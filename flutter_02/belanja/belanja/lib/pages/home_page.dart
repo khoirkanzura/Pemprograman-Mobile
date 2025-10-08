fc5c3bd721c1976b87941acba0f6ec33c796ee18
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-10-08 07:06:11 +0700

    Praktikum 5: Membangun Navigasi di Flutter - Langkah 6:  Perbaikan

--- flutter_02/belanja/belanja/lib/pages/home_page.dart
+++ flutter_02/belanja/belanja/lib/pages/home_page.dart
@@ -6,11 +6,41 @@ class HomePage extends StatelessWidget {
 
   final List<Item> items = [
     Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000)
+    Item(name: 'Salt', price: 2000),
   ];
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping List'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return Card(
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(child: Text(item.name)),
+                    Expanded(
+                      child: Text(
+                        item.price.toString(),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
