c45d739dd348176e55c1781ff07df025d400f982
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-10-29 14:45:43 +0700

    Langkah 7: buat method _buildAddTaskButton(), Langkah 8: buat widget _buildList(), Langkah 9: buat widget _buildTaskTile

--- Dasar_State_Management/master_plan/lib/models/data_layer.dart
+++ Dasar_State_Management/master_plan/lib/models/data_layer.dart
@@ -1,2 +1,27 @@
 export 'plan.dart';
 export 'task.dart';
+
+import 'package:flutter/foundation.dart';
+
+@immutable
+class Task {
+  final String description;
+  final bool complete;
+
+  const Task({
+    this.description = '',
+    this.complete = false,
+  });
+}
+
+@immutable
+class Plan {
+  final String name;
+  final List<Task> tasks;
+
+  const Plan({
+    this.name = '',
+    this.tasks = const [],
+  });
+}
+
--- Dasar_State_Management/master_plan/lib/views/plan_screen.dart
+++ Dasar_State_Management/master_plan/lib/views/plan_screen.dart
@@ -14,9 +14,67 @@ class _PlanScreenState extends State<PlanScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Master Plan Khoirrr')), // Ganti dengan nama kamu
+      appBar: AppBar(title: const Text('Master Plan Khoirrr')), 
       body: _buildList(),
       floatingActionButton: _buildAddTaskButton(),
+    );
+  }
+
+  Widget _buildAddTaskButton() {
+    return FloatingActionButton(
+      child: const Icon(Icons.add),
+      onPressed: () {
+        setState(() {
+          // 
+          plan = Plan(
+            name: plan.name,
+            tasks: List<Task>.from(plan.tasks)..add(const Task()),
+          );
+        });
+      },
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      itemCount: plan.tasks.length,
+      itemBuilder: (context, index) =>
+          _buildTaskTile(plan.tasks[index], index),
+    );
+  }
+
+  Widget _buildTaskTile(Task task, int index) {
+    return ListTile(
+      leading: Checkbox(
+        value: task.complete,
+        onChanged: (selected) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: task.description,
+                  complete: selected ?? false,
+                ),
+            );
+          });
+        },
+      ),
+      title: TextFormField(
+        initialValue: task.description,
+        onChanged: (text) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: text,
+                  complete: task.complete,
+                ),
+            );
+          });
+        },
+      ),
     );
   }
 }
