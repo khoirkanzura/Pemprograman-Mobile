1c622d82126f50a9e16554b553ff271252af016b
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-10-29 14:15:10 +0700

    Langkah 6: buat plan_screen.dart

---
+++ Dasar_State_Management/master_plan/lib/views/plan_screen.dart
@@ -0,0 +1,22 @@
+import '../models/data_layer.dart';
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatefulWidget {
+  const PlanScreen({super.key});
+
+  @override
+  State createState() => _PlanScreenState();
+}
+
+class _PlanScreenState extends State<PlanScreen> {
+  Plan plan = const Plan();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Master Plan Khoirrr')), // Ganti dengan nama kamu
+      body: _buildList(),
+      floatingActionButton: _buildAddTaskButton(),
+    );
+  }
+}
